950ab205a491e4ca1d746113c52e9678193b36ce
Sarah Tiefert <sarah.tiefert@htwg-konstanz.de>
2022-06-26 18:24:05 +0200

    made Deckbuilder work with futures

--- src/main/scala/model/gameComponent/Deckgenerator.scala
+++ src/main/scala/model/gameComponent/Deckgenerator.scala
@@ -1,17 +1,23 @@
 package model.gameComponent
 
-import java.util.concurrent.Future
+
+import scala.util.{Failure, Success}
+import scala.concurrent.{Future, Await}
+import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class Deckgenerator {
 
   def createDeckWithFutures(nuOfCards:Int = 60, startFrom: Int = 1): Deck = {
     
-    /*
+    if (nuOfCards >= 30){
     val f1 = Future{createDeckWithFutures(nuOfCards = (nuOfCards/2), startFrom= (nuOfCards/2))}
     val f2 = Future{createDeckWithFutures((nuOfCards/2), startFrom = 1)}
-    */
-    
-    createRandomDeck(nuOfCards)
+    val deck1 = Await.result(f1, 20 nanos)
+    val deck2 = Await.result(f2, 20 nanos)
+    deck1.addCard(deck2)
+    } else createRandomDeck(nuOfCards, startFrom)
   }
 
   def createRandomDeck(nuOfCards:Int = 30, startFrom: Int = 1): Deck ={
@@ -19,9 +25,6 @@ class Deckgenerator {
     if(nuOfCards > 1) Deck(List(card)).addCard(createRandomDeck(nuOfCards-1))
     else Deck(List(card))
   }
-
-  // futures: if cards is more than 30, split up deck generation to make it faster
-
 
   // TODO: add ability to create proper deck from file
   // TODO: add ability to choose deck
